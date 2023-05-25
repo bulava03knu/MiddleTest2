345e156ed782f268bdc85beebb8dcfc7a8aae866
Hennadii Bulava <bulava03@knu.ua>
2023-05-25 09:43:09 +0300

    Updated tests

--- recipe/tests.py
+++ recipe/tests.py
@@ -5,31 +5,17 @@ from .models import Recipe, Category
 class ViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
+        self.category = Category.objects.create(name='Dessert')
+        self.recipe = Recipe.objects.create(title='Chocolate Cake', description='Delicious chocolate cake',
+                                             instructions='Bake it', ingredients='chocolate, flour, sugar, eggs',
+                                             category=self.category)
 
     def test_main_view(self):
-        # Створення тестових рецептів
-        category = Category.objects.create(name="Test Category")
-        for i in range(6):
-            Recipe.objects.create(title=f"Test Recipe {i}", description="Test Description",
-                                  instructions="Test Instructions", ingredients="Test Ingredients",
-                                  category=category)
-
-        response = self.client.get('')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context['recipes']), 5)
+        self.assertContains(response, 'Chocolate Cake')
 
     def test_category_list_view(self):
-        # Створення тестових категорій
-        for i in range(5):
-            category = Category.objects.create(name=f"Test Category {i}")
-            for j in range(i+1):
-                Recipe.objects.create(title=f"Test Recipe {j}", description="Test Description",
-                                      instructions="Test Instructions", ingredients="Test Ingredients",
-                                      category=category)
-
         response = self.client.get('/categories/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context['categories']), Category.objects.count())
-
-        for category in response.context['categories']:
-            self.assertEqual(category.recipe_count, Recipe.objects.filter(category=category).count())
+        self.assertContains(response, 'Dessert')
